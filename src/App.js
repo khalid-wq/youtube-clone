a0d0c0fc0077b31d01a1248322e6e51b4f32ccbc
khalid ismail <khalidsmail50@gmail.com>
2022-12-07 17:52:35 +0100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route}  from 'react-router-dom'
+import {HashRouter, Routes, Route}  from 'react-router-dom'
 import {Box} from '@mui/material'
 
 import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
 const App = () => ( 
-    <BrowserRouter>
+    <HashRouter>
     <Box sx={{backgroundColor: '#000'}}>
     <Navbar/>
     <Routes>
-        <Route path="/" exact element={<Feed/>}/>
+        <Route path="/" exact element={<Feed/>}/>        
         <Route path='/video/:id' element={<VideoDetail/>}/>
         <Route path='/channel/:id' element={<ChannelDetail/>}/>
         <Route path='/search/:searchterm' element={<SearchFeed/>}/>
     </Routes>
     </Box>
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default App
